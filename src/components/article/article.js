849bdcd4f5dc9d9531b13076f827a68fa4a373fe
kiryu <13matheusilva@gmail.com>
2024-05-27 12:10:47 -0300

    article component add

---
+++ src/components/article/article.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Article = (prop) => {
+    return (
+        <div className="article">
+            <img src={prop.src} alt={prop.title}></img>
+            <div className="info">
+                <p className="article-author">{prop.author}</p>
+                <a className="article-title" href="/">{prop.title}</a>
+                <p className="article-description">{prop.description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Article
